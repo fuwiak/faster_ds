676e9e7efc332ac67a6e317bd4a9649e4f173bb0
Macbook SSD <poczta130@gmail.com>
2019-12-10 20:46:21 +0300

    test_split

--- classification.py
+++ classification.py
@@ -15,21 +15,35 @@ class model:
 
 	def set_X(self, X_name):
 		# assert type(X_name) == 'str'
+		#X_name have to be a list
 		df = pd.read_csv(self.data)
 
 		return df[X_name]
 
-	def set_Y():
-		pass
+	def set_Y(self, Y_name):
+		df = pd.read_csv(self.data)
 
-	def test_train(self, ratio):
-		pass
+		return df[Y_name]
+
+	def test_train(self, ratio,random_state=100):
+		from sklearn.model_selection import train_test_split
+
+		train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=ratio, random_state)
+		return train_X, test_X, train_y, test_y 
 
 	def set_algorithm_name():
+		#list_of_algo
 		pass
 
 	def plot_roc_curve():
 		pass
+
+	def plot_log_loss():
+		pass
+
+	def plot_acc_epoch():
+		pass
+
 
 	def confusion_matrix():
 		pass
@@ -49,7 +63,13 @@ class model:
 
 	def show_random_time():
 		pass
-	
+
+	def dump_to_pickle():
+		pass
+
+	def read_pickle():
+		pass
+
 
 
 
