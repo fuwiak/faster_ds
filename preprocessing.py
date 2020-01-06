7465f05a32e10deebb462801e972e3bd1fc9bb22
Macbook SSD <poczta130@gmail.com>
2020-01-06 02:11:44 +0100

    preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+
+def is_missing(df):
+    "Determine if ANY Value in a Series is Missing"
+    x = df.isnull().values.any()
+    print(x)
+
+
+def count_missing(df, total=True):
+	"Count Missing Values in DataFrame"
+
+	if total:
+		print(df.isnull().sum().sum())
+	else:
+		#by column
+		print(print(df.isnull().sum())
