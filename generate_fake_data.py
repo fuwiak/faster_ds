6a62bea5beb034e5ce43ccbffb0a00e02c5491ea
Macbook SSD <poczta130@gmail.com>
2020-02-18 14:31:07 +0100

    one_sentence added

--- generate_fake_data.py
+++ generate_fake_data.py
@@ -1,6 +1,5 @@
 from faker import Faker
-fake = Faker()
-
+import pandas as pd
 
 
 '''
@@ -9,13 +8,46 @@ class which generates fake and sample data
 
 '''
 
-
-
-
-
-
 class fake_data:
-	pass
+
+	@staticmethod
+	def one_sentence():
+		 """
+	    Returns text(str)
+	    Parameters
+	    -----------
+	    
+	    
+	        No parameters
+	    
+	  
+
+    	"""
+		temp = Faker()
+		return temp.text()
+
+	@staticmethod
+	def many_sentences(how_many):
+		 """
+	    
+	    Returns Pandas Dataframe with text
+	    
+	    Parameters
+	    -----------
+	    
+	    how_many
+	        number of rows of text in dataframe
+	    """
+
+
+		temp = Faker()
+		data = []
+		for i in range(how_many):
+			data.append(temp.text())
+
+		return pd.DataFrame(data, columns=["text"])
+
+
 
 
 	def classfication_data(self, *params):
