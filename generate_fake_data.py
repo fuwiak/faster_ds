8b96604e22280fb1cec210f0fcdf3778f704f1f7
Macbook SSD <poczta130@gmail.com>
2020-02-18 16:24:36 +0100

    classification and regression fake data

--- generate_fake_data.py
+++ generate_fake_data.py
@@ -1,10 +1,11 @@
 from faker import Faker
 import pandas as pd
-
+import datetime
+import numpy as np
 
 '''
 
-class which generates fake and sample data
+	class which generates fake and sample data
 
 '''
 
@@ -12,34 +13,31 @@ class fake_data:
 
 	@staticmethod
 	def one_sentence():
-		 """
-	    Returns text(str)
+		"""
+	    
+	    Returns text(string)
+	    
 	    Parameters
 	    -----------
 	    
-	    
-	        No parameters
-	    
-	  
-
-    	"""
+	    """
+		
 		temp = Faker()
 		return temp.text()
-
 	@staticmethod
 	def many_sentences(how_many):
-		 """
+		"""
 	    
 	    Returns Pandas Dataframe with text
 	    
 	    Parameters
 	    -----------
-	    
 	    how_many
-	        number of rows of text in dataframe
+	    	number of rows of text in dataframe
+
+	    
 	    """
-
-
+		
 		temp = Faker()
 		data = []
 		for i in range(how_many):
@@ -47,16 +45,101 @@ class fake_data:
 
 		return pd.DataFrame(data, columns=["text"])
 
+	@staticmethod
+	def classfication_data(how_many):
+		"""
+		Returns Pandas Dataframe with data to classification
+
+		Parameters
+		-----------
+		how_many
+			number of rows of text in dataframe
 
 
+		"""
+		data=[]
+		for i in range(how_many):
+			temp= Faker('en_US')
+			row = [
 
-	def classfication_data(self, *params):
-		pass
+			temp.prefix(),
+			temp.name(),
+			temp.date(pattern="%d-%m-%Y", end_datetime=datetime.date(2020, 1,1)),
+			temp.phone_number(),
+			temp.email(),
+			temp.address(),
+			temp.zipcode(),
+			temp.city(),
+			temp.state(),
+			temp.country(),
+			temp.year(),
+			temp.time(),
+			temp.url(),
+			np.random.randint(0,2,1)[0]
+			]
+			data.append(row)
+
+		headers = ["Prefix", "Name", "Birth Date", "Phone Number", "Additional Email Id",
+		"Address", "Zip Code", "City","State", "Country", "Year", "Time", "Link", "HaveAjob"]
+
+		df = pd.DataFrame(data, columns=headers)
+		return df
+	@staticmethod
+	def regression_data(how_many):	
+		"""
+		Returns Pandas Dataframe with data to regression
+
+		Parameters
+		-----------
+		how_many
+			number of rows of text in dataframe
 
 
-	def regression_data(self, *params):
-		pass
+		"""
+		data=[]
+		for i in range(how_many):
+			temp= Faker('en_US')
+			row = [
 
+			temp.prefix(),
+			temp.name(),
+			temp.date(pattern="%d-%m-%Y", end_datetime=datetime.date(2020, 1,1)),
+			temp.phone_number(),
+			temp.email(),
+			temp.address(),
+			temp.zipcode(),
+			temp.city(),
+			temp.state(),
+			temp.country(),
+			temp.year(),
+			temp.time(),
+			temp.url(),
+			np.random.randint(1000,20000,1)[0]
+			]
+			data.append(row)
 
+		headers = ["Prefix", "Name", "Birth Date", "Phone Number", "Additional Email Id",
+		"Address", "Zip Code", "City","State", "Country", "Year", "Time", "Link", "Salary"]
+
+		df = pd.DataFrame(data, columns=headers)
+		return df
+
+	@staticmethod
 	def clasterization_data(self, *params):
-		pass
+		from sklearn.datasets.samples_generator import make_blobs
+
+		centers = [(-5, -5), (5, 5)]
+		cluster_std = [0.8, 1]
+
+		X, y = make_blobs(n_samples=100, cluster_std=cluster_std, centers=centers, n_features=2, random_state=1)
+
+		plt.scatter(X[y == 0, 0], X[y == 0, 1], color="red", s=10, label="Cluster1")
+		plt.scatter(X[y == 1, 0], X[y == 1, 1], color="blue", s=10, label="Cluster2")
+		plt.show()
+
+
+
+
+
+
+
