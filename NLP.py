cfe451aee9550dda91fd4475354cfd78c4a2067b
Pawel <poczta130@gmail.com>
2020-02-20 14:29:16 +0100

    Update NLP.py

--- NLP.py
+++ NLP.py
@@ -2,21 +2,24 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
 
+class NLP:
+	@staticmethod
+	def vectorize_text(text):
+		vectorizer = CountVectorizer()
+		vectorizer.fit(text)
 
-def vectorize_text(text):
-	vectorizer = CountVectorizer()
-	vectorizer.fit(text)
-									
-	print(vectorizer.vocabulary_)
+		print(vectorizer.vocabulary_)
 
-	# Transform text to vector
-	vector = vectorizer.transform(text)
-	print(vector.shape)
-	print(type(vector))
-	print(vector.toarray())
+		# Transform text to vector
+		vector = vectorizer.transform(text)
+		print(vector.shape)
+		print(type(vector))
+		print(vector.toarray())
 
 
-
+	
+'''
 if __name__ == "__main__":
 	text = "Sample text to check correctness of program "
 	vectorize_text(text)
+'''
