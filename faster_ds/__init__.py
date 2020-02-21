748ecc55fb6f38fd009890ebe237c57aff75f414
Macbook SSD <poczta130@gmail.com>
2020-02-21 12:15:01 +0100

    update faster_ds/

--- faster_ds/__init__.py
+++ faster_ds/__init__.py
@@ -5,10 +5,10 @@ Package Description.
 """
 
 __version__ = "0.0.1"
-__short_description__ = "Package short description."
+__short_description__ = "Numpy/Pandas based module make faster data analysis"
 __license__ = "MIT"
 __author__ = "fuwiak"
-__author_email__ = "author@example.com"
+__author_email__ = "poczta130@gmail.com"
 __maintainer__ = "unknown maintainer"
 __maintainer_email__ = "maintainer@example.com"
 __github_username__ = "fuwiak"
