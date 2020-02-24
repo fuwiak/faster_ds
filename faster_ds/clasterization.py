c82d1b388be801918bad1b877e464e934697b9a8
Pawel <poczta130@gmail.com>
2020-02-24 13:20:32 +0100

    Update clasterization.py

--- faster_ds/clasterization.py
+++ faster_ds/clasterization.py
@@ -1,11 +1,36 @@
 from sklearn import preprocessing  # to normalise existing X
 from sklearn import cluster
-X_Norm = preprocessing.normalize(DF)
 
-km2 = cluster.KMeans(n_clusters=5,init='random').fit(X_Norm)
 
-labels = km2.labels_
-labels = np.unique(labels)
+class cluster_model:
+  @staticmethod
+  def data_normalization(X):
+    """
+    X_norm ---> dataframe
+    
+    """
+    X_Norm = preprocessing.normalize(X)
+  return X_Norm
 
-km2.cluster_centers_
-plt.scatter(X_Norm[:,0],X_Norm[:,2], c=km2.labels_, cmap='rainbow')
+  @staticmethod
+  def km_labels(X_norm, n_clusters=5):
+    """
+    X_norm---> normed dataframe
+    """
+    km2 = cluster.KMeans(n_clusters=n_clusters,init='random').fit(X_Norm)
+    labels = km2.labels_
+    labels = np.unique(labels)
+    return labels
+  @staticmethod
+  def km_centers(X_norm, n_clusters=5):
+     """
+    X_norm---> normed dataframe
+    """
+      km2 = cluster.KMeans(n_clusters=n_clusters,init='random').fit(X_Norm)
+      
+      return km2.cluster_centers_
+   def plot_km(X_norm, labels):
+    #labels=km2.labels_
+    
+    plt.scatter(X_Norm[:,0],X_Norm[:,2], c=labels, cmap='rainbow')
+    plt.show()
