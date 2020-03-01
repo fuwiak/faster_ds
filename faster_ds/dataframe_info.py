6bb198d45c887d677395a8d994cf8c3df14b0426
Pawel <poczta130@gmail.com>
2020-03-01 19:15:33 +0100

    Create dataframe_info.py

---
+++ faster_ds/dataframe_info.py
@@ -0,0 +1,25 @@
+def dataframe_metainformation(df):
+    meta = dict()
+    descr = pd.DataFrame({'dtype': df.dtypes, 'NAs': df.isna().sum()})
+    categorical_features = descr.loc[descr['dtype'] == 'object'].index.values.tolist()
+    numerical_features = descr.loc[descr['dtype'] != 'object'].index.values.tolist()
+    numerical_features_na = descr.loc[(descr['dtype'] != 'object') & (descr['NAs'] > 0)].index.values.tolist()
+    categorical_features_na = descr.loc[(descr['dtype'] == 'object') & (descr['NAs'] > 0)].index.values.tolist()
+    complete_features = descr.loc[descr['NAs'] == 0].index.values.tolist()
+    meta['description'] = descr
+    meta['categorical_features'] = categorical_features
+    meta['categorical_features'] = categorical_features
+    meta['categorical_features_na'] = categorical_features_na
+    meta['numerical_features'] = numerical_features
+    meta['numerical_features_na'] = numerical_features_na
+    meta['complete_features'] = complete_features
+    return meta
+
+def print_metainformation(meta):
+    print('Available types:', meta['description']['dtype'].unique())
+    print('{} Features'.format(meta['description'].shape[0]))
+    print('{} categorical features'.format(len(meta['categorical_features'])))
+    print('{} numerical features'.format(len(meta['numerical_features'])))
+    print('{} categorical features with NAs'.format(len(meta['categorical_features_na'])))
+    print('{} numerical features with NAs'.format(len(meta['numerical_features_na'])))
+    print('{} Complete features'.format(len(meta['complete_features'])))
