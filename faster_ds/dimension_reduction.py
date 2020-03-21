da59c420aee186e13c211c5facc55ef98f750f72
Pawel <poczta130@gmail.com>
2020-03-21 13:15:05 +0100

    Update dimension_reduction.py

--- faster_ds/dimension_reduction.py
+++ faster_ds/dimension_reduction.py
@@ -30,15 +30,15 @@ class PCA:
 		ax.set_xlabel('Principal Component 1', fontsize = 15)
 		ax.set_ylabel('Principal Component 2', fontsize = 15)
 		ax.set_title('2 component PCA', fontsize = 20)
-		targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-		colors = ['r', 'g', 'b']
-		for target, color in zip(targets,colors):
-		    indicesToKeep = finalDf['target'] == target
-		    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-			       , finalDf.loc[indicesToKeep, 'principal component 2']
-			       , c = color
-			       , s = 50)
-		ax.legend(targets)
-		ax.grid()
+# 		targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+# 		colors = ['r', 'g', 'b']
+# 		for target, color in zip(targets,colors):
+# 		    indicesToKeep = finalDf['target'] == target
+# 		    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+# 			       , finalDf.loc[indicesToKeep, 'principal component 2']
+# 			       , c = color
+# 			       , s = 50)
+# 		ax.legend(targets)
+# 		ax.grid()
 
 		return pca.explained_variance_ratio_
