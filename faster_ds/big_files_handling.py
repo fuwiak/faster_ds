4e86093007557879f26374811ceae171fa3d066f
Pawel <poczta130@gmail.com>
2020-03-28 22:33:52 +0100

    Update big_files_handling.py

--- faster_ds/big_files_handling.py
+++ faster_ds/big_files_handling.py
@@ -1,4 +1,8 @@
 
+def get_info(df):
+    return df.info
+
+
 def num_megabytes(df):
     return sum(df.memory_usage()/1024**2)
 
@@ -9,3 +13,10 @@ def reduce_to_int16(X):
           X[col] = X[col].astype(np.int16)
       if X[col].dtype==np.float64:
           X[col] = X[col].astype(np.float16)
+   return X
+
+def reduce_to_category(df):
+    df = df.select_dtypes(include=['object']).copy()
+    df=df.astype('category')
+    return df
+    
