9d6459534e6a60fdca5e193206ff8cd0dff2b267
Pawel <poczta130@gmail.com>
2020-06-12 13:22:33 +0200

    Create regression.py

---
+++ faster_ds/regression.py
@@ -0,0 +1,9 @@
+
+class RegreessionInterface:
+    def method1(self, path: str, file_name: str) -> str:
+         """method description"""
+        pass
+
+    def method2(self, full_file_name: str) -> dict:
+        """method description"""
+        pass
