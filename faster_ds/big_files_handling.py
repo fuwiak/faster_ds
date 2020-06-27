071e28c1e2d66af374fb36e683e8c55961801978
Pawel <poczta130@gmail.com>
2020-06-28 00:06:38 +0200

    Update big_files_handling.py

--- faster_ds/big_files_handling.py
+++ faster_ds/big_files_handling.py
@@ -1,22 +1,29 @@
+import numpy as np
+import pandas as pd
 
-def get_info(df):
-    return df.info
 
+class BigFiles:
+    
+    @staticmethod
+    def get_info(df):
+        return df.info
 
-def num_megabytes(df):
-    return sum(df.memory_usage()/1024**2)
-
-  
-def reduce_to_int16(X):
-  for col in X.columns:
-      if X[col].dtype==np.int64:
-          X[col] = X[col].astype(np.int16)
-      if X[col].dtype==np.float64:
-          X[col] = X[col].astype(np.float16)
-   return X
-
-def reduce_to_category(df):
-    df = df.select_dtypes(include=['object']).copy()
-    df = df.astype('category')
-    return df
+    @staticmethod
+    def num_megabytes(df):
+        return sum(df.memory_usage()/1024**2)
+    
+    @staticmethod
+    def reduce_to_int16(X):
+      for col in X.columns:
+          if X[col].dtype==np.int64:
+              X[col] = X[col].astype(np.int16)
+          if X[col].dtype==np.float64:
+              X[col] = X[col].astype(np.float16)
+       return X
+    
+    @staticmethod
+    def reduce_to_category(df):
+        df = df.select_dtypes(include=['object']).copy()
+        df = df.astype('category')
+        return df
     
