8869e78f275d29d4a6562dbb4f493a941bc103ec
Pawel <poczta130@gmail.com>
2020-06-28 00:27:20 +0200

    Update handling_imbalanced.py

--- faster_ds/handling_imbalanced.py
+++ faster_ds/handling_imbalanced.py
@@ -1,12 +1,13 @@
 class HandlingImbalanced:
   @staticmethod
-  def smote(X,y);
+  def smote(X,y):
     """
     SMOTE (Synthetic Minority Oversampling TEchnique) consists of synthesizing elements for the minority class, 
     based on those that already exist. It works randomly picingk a point from the minority class 
     and computing the k-nearest neighbors for this point. 
     The synthetic points are added between the chosen point and its neighbors.
     """
+    
     from imblearn.over_sampling import SMOTE
     smote = SMOTE(ratio='minority')
     X_sm, y_sm = smote.fit_sample(X, y)
