3b19619d030769ac1e3d1685705899dd70224f38
Pawel <poczta130@gmail.com>
2020-06-28 00:11:29 +0200

    Update big_files_handling.py

--- faster_ds/big_files_handling.py
+++ faster_ds/big_files_handling.py
@@ -14,12 +14,12 @@ class BigFiles:
     
     @staticmethod
     def reduce_to_int16(X):
-      for col in X.columns:
-          if X[col].dtype==np.int64:
-              X[col] = X[col].astype(np.int16)
-          if X[col].dtype==np.float64:
-              X[col] = X[col].astype(np.float16)
-       return X
+        for col in X.columns:
+            if X[col].dtype==np.int64:
+                X[col] = X[col].astype(np.int16)
+            if X[col].dtype==np.float64:
+                X[col] = X[col].astype(np.float16)
+        return X
     
     @staticmethod
     def reduce_to_category(df):
