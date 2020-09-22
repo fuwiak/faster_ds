8592a1395fb40c531e09ddc4c130ab039ab1512d
fuwiak <poczta130@gmail.com>
2020-09-22 18:21:05 +0200

    create ML Processing Tools

--- faster_ds/regression.py
+++
@@ -1,9 +0,0 @@
-
-class RegreessionInterface:
-    def method1(self, path: str, file_name: str) -> str:
-         """method description"""
-        pass
-
-    def method2(self, full_file_name: str) -> dict:
-        """method description"""
-        pass
