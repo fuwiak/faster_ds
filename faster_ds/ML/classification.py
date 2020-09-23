45038b535a9213751ffb9bbdae91aabfea4981eb
fuwiak <poczta130@gmail.com>
2020-09-23 15:58:36 +0200

    update classification

--- faster_ds/ML/classification.py
+++ faster_ds/ML/classification.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 
-class model:
+class Model:
+
+	def __init__(self, clf, model_name):
+		self.clf = clf
+		self.model_name = model_name
+
 
 	@staticmethod
 	def clf_details(clf):
@@ -36,50 +41,50 @@ class model:
 		plt.legend()
 		plt.show()
 
-	@staticmethod
-	def plot_acc_epoch():
-		""" Home-made mini-batch learning
-		    -> not to be used in out-of-core setting!
-		"""
-		N_TRAIN_SAMPLES = X_train.shape[0]
-		N_EPOCHS = 25
-		N_BATCH = 128
-		N_CLASSES = np.unique(y_train)
+	# @staticmethod
+	# def plot_acc_epoch():
+	# 	""" Home-made mini-batch learning
+	# 	    -> not to be used in out-of-core setting!
+	# 	"""
+	# 	N_TRAIN_SAMPLES = X_train.shape[0]
+	# 	N_EPOCHS = 25
+	# 	N_BATCH = 128
+	# 	N_CLASSES = np.unique(y_train)
 
-		scores_train = []
-		scores_test = []
+	# 	scores_train = []
+	# 	scores_test = []
 
-			epoch = 0
-			while epoch < N_EPOCHS:
-			    print('epoch: ', epoch)
-			    # SHUFFLING
-			    random_perm = np.random.permutation(X_train.shape[0])
-			    mini_batch_index = 0
-			    while True:
-				# MINI-BATCH
-				indices = random_perm[mini_batch_index:mini_batch_index + N_BATCH]
-				mlp.partial_fit(X_train[indices], y_train[indices], classes=N_CLASSES)
-				mini_batch_index += N_BATCH
+	# 		epoch = 0
+	# 		while epoch < N_EPOCHS:
+	# 		    print('epoch: ', epoch)
+	# 		    # SHUFFLING
+	# 		    random_perm = np.random.permutation(X_train.shape[0])
+	# 		    mini_batch_index = 0
+	# 		    while True:
+	# 			# MINI-BATCH
+	# 			indices = random_perm[mini_batch_index:mini_batch_index + N_BATCH]
+	# 			mlp.partial_fit(X_train[indices], y_train[indices], classes=N_CLASSES)
+	# 			mini_batch_index += N_BATCH
 
-				if mini_batch_index >= N_TRAIN_SAMPLES:
-				    break
+	# 			if mini_batch_index >= N_TRAIN_SAMPLES:
+	# 			    break
 
-			    # SCORE TRAIN
-			    scores_train.append(mlp.score(X_train, y_train))
+	# 		    # SCORE TRAIN
+	# 		    scores_train.append(mlp.score(X_train, y_train))
 
-			    # SCORE TEST
-			    scores_test.append(mlp.score(X_test, y_test))
+	# 		    # SCORE TEST
+	# 		    scores_test.append(mlp.score(X_test, y_test))
 
-			    epoch += 1
+	# 		    epoch += 1
 
-			""" Plot """
-			fig, ax = plt.subplots(2, sharex=True, sharey=True)
-			ax[0].plot(scores_train)
-			ax[0].set_title('Train')
-			ax[1].plot(scores_test)
-			ax[1].set_title('Test')
-			fig.suptitle("Accuracy over epochs", fontsize=14)
-			plt.show()
+	# 		""" Plot """
+	# 		fig, ax = plt.subplots(2, sharex=True, sharey=True)
+	# 		ax[0].plot(scores_train)
+	# 		ax[0].set_title('Train')
+	# 		ax[1].plot(scores_test)
+	# 		ax[1].set_title('Test')
+	# 		fig.suptitle("Accuracy over epochs", fontsize=14)
+	# 		plt.show()
 
 	@staticmethod
 	def confusion_matrix(clf, train_X, train_y, test_y,test_X):
