695c02b0fe6f1dff65f16b2b7cb2a9afbe2f6cdb
Pawel <poczta130@gmail.com>
2021-01-13 16:40:43 +0100

    add show_metrics

--- faster_ds/ML/regression.py
+++ faster_ds/ML/regression.py
@@ -1,9 +1,21 @@
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
 
-class RegreessionInterface:
-    def method1(self, path: str, file_name: str) -> str:
-         """method description"""
-        pass
 
-    def method2(self, full_file_name: str) -> dict:
-        """method description"""
-        pass
+
+# class RegreessionInterface:
+#     def method1(self, path: str, file_name: str) -> str:
+#          """method description"""
+#         pass
+
+#     def method2(self, full_file_name: str) -> dict:
+#         """method description"""
+#         pass
+    
+
+#metrics
+
+def show_metrics(y_true, y_pred):
+    pass
+    
