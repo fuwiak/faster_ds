f1f214b922398b7af3b799e33229a358fc50bb55
Pawel <poczta130@gmail.com>
2021-04-17 22:13:47 +0100

    update def: dump_to_pickle, load_from_pickle

--- faster_ds/Tools/model_to_production.py
+++ faster_ds/Tools/model_to_production.py
@@ -5,12 +5,10 @@ import pickle
 class ToProduction:
   
 	@staticmethod
-	def dump_to_pickle(clf, filename):
-		import pickle
+	def dump_to_pickle(clf, filename: str)-> None:
 		pickle.dump(model, open(filename, 'wb'))
 
 	@staticmethod
-	def load_from_pickle(clf, filename):
-		import pickle
+	def load_from_pickle(clf, filename: str):
 		loaded_model = pickle.load(open(filename, 'rb'))
 		return loaded_model
