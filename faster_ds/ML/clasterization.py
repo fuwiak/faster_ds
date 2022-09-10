6a4e9c995fb52de40f48ccc5ca33f6da1947f445
Леонид Каретников <poczta130@gmail.com>
2022-09-10 12:59:59 +0100

    Update clasterization.py

--- faster_ds/ML/clasterization.py
+++ faster_ds/ML/clasterization.py
@@ -4,20 +4,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
 
-# class CusterInterface:
-#     def method1(self, path: str, file_name: str) -> str:
-#          """method description"""
-#         pass
-
-#     def method2(self, full_file_name: str) -> dict:
-#         """method description"""
-#         pass
-
-
 
 class ClusterModel:
+	"""
+	:param path: path to the file
+	:param file_name: file name
+	:param full_file_name: full file name
+	:param df: dataframe
+	:param X: data
+	:param y: labels
+	:param X_train: train data
+	:param X_test: test data
+	:param y_train: train labels
+	:param y_test: test labels
+
+	"""
+	def __init__(self, path, file_name, full_file_name, df, X, y, X_train, X_test, y_train, y_test):
+		self.path = path
+		self.file_name = file_name
+		self.full_file_name = full_file_name
+		self.df = df
+		self.X = X
+		self.y = y
+		self.X_train = X_train
+		self.X_test = X_test
+		self.y_train = y_train
+		self.y_test = y_test
+	
 	@staticmethod
-	def data_normalization(X):
+	def data_normalization(X: pd.DataFrame) -> pd.DataFrame:
 		"""
 
 		normalize dataframe
@@ -66,8 +81,12 @@ class ClusterModel:
 	@staticmethod
 	def km_centers(X_norm, n_clusters=5):
 		"""
-		returns k-means centroids
-		
+		return k-means centers
+		Parameters
+		----------
+		X_norm: dataframe like, array
+		n_clusters: int
+
 
 		"""
 
@@ -77,7 +96,15 @@ class ClusterModel:
 
 
 	@staticmethod
-	def predict_labels(X ,n_clusters=5):
+	def predict_labels(X_norm,n_clusters=5):
+		"""
+		returns k-means labels for new data
+		Parameters
+		----------
+		X_norm: dataframe like, array
+		n_clusters: int
+
+		"""
 		km2 = cluster.KMeans(n_clusters=n_clusters,init='random')
 		y_km2 = km2.fit_predict(X)
 		return y_km2
@@ -86,18 +113,30 @@ class ClusterModel:
 
 	@staticmethod
 	def plot_centroids(X_norm, labels):
+		"""
+		plot centroids
+		"""
 		plt.scatter(X_norm[:,0],X_norm[:,1], c=labels, cmap='rainbow',marker='*', label='centroids')
 		plt.show()
 
 	@staticmethod
 	def plot_clusters(X_norm, cluster_number):
-		raise NotImplementedError
-		
-		
-	# plt.scatter(X[y == 0, 0], X[y == 0, 1], color="red", s=10, label="Cluster1")
-		# plt.scatter(X[y == 1, 0], X[y == 1, 1], color="blue", s=10, label="Cluster2")
-		# plt.scatter(X[y == 2, 0], X[y == 2, 1], color="pink", s=10, label="Cluster3")
-		# plt.show()
+		"""
+		plot clusters
+		Parameters
+		----------
+		X_norm: dataframe like, array
+		cluster_number: int
+
+		Returns
+		-------
+		plot of clusters
+
+		"""
+		km2 = cluster.KMeans(n_clusters=cluster_number,init='random')
+		y_km2 = km2.fit_predict(X_norm)
+		plt.scatter(X_norm[:,0],X_norm[:,1], c=y_km2, cmap='rainbow')
+		plt.show()
 
 
 
