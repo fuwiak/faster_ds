825cbea2a1b1489d338376cf2c550f7c7ea5cc16
Леонид Каретников <poczta130@gmail.com>
2022-12-29 01:29:08 +0100

    am

--- templates/estimator_temp.py
+++ templates/estimator_temp.py
@@ -1,7 +1,4 @@
 #https://scikit-learn.org/stable/developers/develop.html
-
-
-
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
